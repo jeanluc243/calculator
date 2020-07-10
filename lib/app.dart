f34d3b5d650d010055854cfcf5201f42424bb92b
Jean Luc Kabulu <jeanluc.kabulu@gmail.com>
2020-07-10 20:55:10 +0200

    Add splashy

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:splashy/splashy.dart';
 
 class App extends StatelessWidget {
-  // This widget is the root of your application.
+  Future<Widget> customFunction() {
+    print("Background process");
+    return Future.value(Scaffold(
+      body: Container(),
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Calculator',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Container(),
+      home: Splashy(
+        imagePath: "assets/images/icon.png",
+        customFunction: customFunction(),
+        logoHeight: 100,
+        logoWidth: 100,
+        duration: 10000,
+        curve: Curves.bounceInOut,
+      ),
     );
   }
 }
